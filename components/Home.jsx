b8fb15700bda87209c6a57207e13074663d88940
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-02-24 11:32:30 +0600

    second layer deaign

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,13 +1,11 @@
 import '../css/main_all.css'
 import { MdArrowOutward } from "react-icons/md";
 import { MdVerified } from "react-icons/md";
-import exp1 from '../src/images/exp1.jpeg'
-import exp2 from '../src/images/exp2.webp'
-
+import { IoSettingsOutline } from "react-icons/io5";
 const Home = () => {
     return (
-        <div className='mb-60'>
-            <div className='overleyImg h-[700px] md:h-[800px] w-full'>
+        <div className='mb-60 Manrope'>
+            <div className='bg-[#C4C4C4] h-[700px] md:h-[800px] w-full'>
                 <div className='w-10/12 mx-auto'>
                     <div>
                         <div className='text-center md:text-start text-white pt-40 md:pt-60'>
@@ -49,15 +47,61 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className='md:mt-10 mt-5 w-8/12 mx-auto'>
-                <div>
-                    <div className=''>
-                        <img className='rounded-3xl w-[300px] h-[208px] ' src={exp1} alt="" />
+            <div className='md:mt-30 mt-5 w-8/12 mx-auto'>
+                <div className='flex gap-30'>
+                    <div>
+                        <div className='flex gap-8'>
+                            <div className='bg-[#C4C4C4] rounded-3xl w-[300px] h-[208px]'></div>
+                            <div className='mt-10 flex gap-8'>
+                                <h1 className='text-[#FF9900] text-5xl manrope font-bold'>25 + </h1>
+                                <div>
+                                    <p className='manrope font-medium w-[90px]'>
+                                        Year Of
+                                        Experience
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='-mt-16 absolute ml-36'>
+                            <div className='w-[300px] bg-[#C4C4C4] h-[360px] border-8 border-white rounded-4xl' alt="" />
+                        </div>
                     </div>
-                    <div className='-mt-16 absolute ml-36'>
-                        <img className='w-[300px] h-[360px] border-8 border-white rounded-4xl' src={exp2} alt="" />
+                    <div>
+                        <div className='flex gap-2 '>
+                            <IoSettingsOutline className='mt-5 text-md text-[#FF9900]'></IoSettingsOutline>
+                            <h1 className=' font-medium tracking-[.20em] mt-4'>About Us</h1>
+                        </div>
+                        <div className=' text-[#222222] mt-6 flex flex-col gap-4 w-[615px]'>
+                            <h1 className='text-5xl font-thin'>Building quality through</h1>
+                            <h1 className='font-bold text-5xl'>industrial innovation</h1>
+                        </div>
+                        <p className='w-[545px] mt-8 text-gray-500 tracking-wide size-20'>
+                            At the heart of our operations is a commitment to delivering
+                            superior products through cutting-edge technology and innovative processes.
+                        </p>
+                        <div className='flex flex-col gap-5 mt-5'>
+                            <div className="flex gap-2 mb-4 md:mb-0">
+                                <MdVerified className=" text-2xl mt-0.5 text-orange-500" />
+                                <h1 className="Manrope text-xl text-[#222222]">
+                                    Sustainable Manufacturing</h1>
+                            </div>
+                            <div className="flex gap-2 mb-4 md:mb-0">
+                                <MdVerified className=" text-2xl mt-0.5 text-orange-500" />
+                                <h1 className="Manrope text-xl text-[#222222]">
+                                    Advanced Automation</h1>
+                            </div>
+                            <div className="flex gap-2 mb-4 md:mb-0">
+                                <MdVerified className=" text-2xl mt-0.5 text-orange-500" />
+                                <h1 className="Manrope text-xl text-[#222222]">
+                                    Efficient Production Processes</h1>
+                            </div>
+                            <div className="flex gap-2 mb-4 md:mb-0">
+                                <MdVerified className=" text-2xl mt-0.5 text-orange-500" />
+                                <h1 className="Manrope text-xl text-[#222222]">
+                                    Reliable Delivery Services</h1>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
             </div>
 
